5c368d27d2829790afadae5d52113788d3f3f25a
Muhammad Ali <133556679+coder-muhammad@users.noreply.github.com>
2025-08-03 16:09:02 +0500

    Fetch data in table from supabase

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+// const { createClient } = require('@supabase/supabase-js')
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://nitvvoupvafskuwgrvuv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pdHZ2b3VwdmFmc2t1d2dydnV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyMTE0NTUsImV4cCI6MjA2OTc4NzQ1NX0.GwGhOo6ii0eBLSToRVENWRfQHelBbzGuAq-8DxgTW3s')
+const supabase = createClient('https://nitvvoupvafskuwgrvuv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pdHZ2b3VwdmFmc2t1d2dydnV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyMTE0NTUsImV4cCI6MjA2OTc4NzQ1NX0.GwGhOo6ii0eBLSToRVENWRfQHelBbzGuAq-8DxgTW3s');
+
+const { data, error } = await supabase
+  .from('characters')
+  .select()
+console.log('Data:', data);
+console.log('Error:', error);
